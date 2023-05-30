6bda973c66e3f7f462c0803d986012f9c97a651f
LeeSeonggeon <92794731+LeeSeonggeon@users.noreply.github.com>
2023-05-30 15:13:11 +0900

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,3 +1,29 @@
+use `DBDBDIB`;
 
+/*Data for the table `members`(회원) */
+insert into `members` values
+();
 
+/*Date for the table `contents`(콘텐츠)*/
+insert into `contents` values
+();
 
+/*Data for the table `reviews`(리뷰) */
+insert into `reviews`(`content_id`, `writer`, `review`, `time`, `good`, `bad`) values
+();  
+
+/*Date for the table `ratings`(리뷰)*/
+insert into `ratings` values
+();
+
+/*Date for the table `events`(이벤트)*/
+insert into `events`(`name`, `start`, `end`, `condition`, `detail`) values
+();
+
+/*Date for the table `platforms`(플랫폼)*/
+insert into `platforms` values
+();
+
+/*Date for the table `platform_events`(플랫폼 이벤트)*/
+insert into `platform_events`(`platform_name`, `name`, `start`, `start`, `condition`, `detail`) values
+();
