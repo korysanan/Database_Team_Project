e8054ffdb5fe0bbb6b92e70f7b7f3879e95eaedf
korysanan <ujin0561@naver.com>
2023-06-04 21:51:54 +0900

    users 테이블 삭제

--- 데베설/sql문/dbdbdib.sql
+++ 데베설/sql문/dbdbdib.sql
@@ -113,25 +113,6 @@ create table event
     UNIQUE (event_name)
 );
 
-create table users
-(
-	user_nickname varchar(20) NOT NULL,
-	user_id varchar(20) NOT NULL,
-	pw varchar(20) NOT NULL,
-    email varchar(20) NOT NULL,
-    register_day TIMESTAMP NOT NULL,
-    recent_connect TIMESTAMP NOT NULL,
-    genre varchar(20),
-    register_yn BOOL NOT NULL,
-    wave_yn BOOL NOT NULL,
-    netflix_yn BOOL NOT NULL,
-	disney_yn BOOL NOT NULL,
-    tving_yn BOOL NOT NULL,
-    FOREIGN KEY (genre) REFERENCES contents_genre(genre),
-    PRIMARY KEY(user_id),
-    UNIQUE (user_nickname)
-);
-
 CREATE TABLE platforms
 (
 	`name` varchar(50) DEFAULT '' NOT NULL,
