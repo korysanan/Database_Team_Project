8cba180ab1ed8390fcb4499b24f303ec03700a10
hsgo2430 <hsgo2430@naver.com>
2023-06-04 20:06:06 +0900

    revise_genre

--- 고효석/prefer_genre.sql
+++ 고효석/prefer_genre.sql
@@ -1,5 +1,5 @@
 create table prefer_genre (
-    user_num int not null auto_increment,
+    user_num int,
     genre varchar(50),
     primary key(user_num),
     foreign key(user_num) references members(user_num)
